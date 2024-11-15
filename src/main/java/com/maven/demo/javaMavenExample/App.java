4b710b50c2dd8afa71e1d02930eb525de1c459ac
Sanjeevasanku <sanjeevasanku1@gmail.com>
2024-11-15 11:07:18 +0530

    Java Maven project

---
+++ src/main/java/com/maven/demo/javaMavenExample/App.java
@@ -0,0 +1,10 @@
+package com.maven.demo.javaMavenExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Sanjeeva 22BD1A053L");
+    }
+}
